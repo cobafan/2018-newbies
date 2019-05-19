d8828d305b41ac663b57e9cedfeb4393c7e7064b
cobafan <akitaka0405@gmail.com>
2019-05-19 15:55:53 +0900

    fix factory

--- nova/spec/factories/remit_requests.rb
+++ nova/spec/factories/remit_requests.rb
@@ -2,8 +2,26 @@
 
 FactoryBot.define do
   factory :remit_request do
-    user { create(:user) }
-    requested_user { create(:user) }
-    amount 100
+    user { build(:user) }
+    target { build(:user) }
+    amount { 100 }
+
+    trait :outstanding do
+      accepted_at { nil }
+      rejected_at { nil }
+      canceled_at { nil }
+    end
+
+    trait :accepted do
+      accepted_at { Time.current }
+    end
+
+    trait :rejected do
+      rejected_at { Time.current }
+    end
+
+    trait :canceled do
+      canceled_at { Time.current }
+    end
   end
 end
