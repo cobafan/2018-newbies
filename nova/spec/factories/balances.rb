1d5453c740724659a3213e2669cb31b5be2d07fe
cobafan <akitaka0405@gmail.com>
2019-06-19 04:43:16 +0900

    fix factory

--- nova/spec/factories/balances.rb
+++ nova/spec/factories/balances.rb
@@ -3,6 +3,6 @@
 FactoryBot.define do
   factory :balance do
     user { create(:user) }
-    amount 1000
+    amount { 1000 }
   end
 end
--- nova/spec/factories/remit_request_results.rb
+++ nova/spec/factories/remit_request_results.rb
@@ -4,18 +4,18 @@ FactoryBot.define do
   factory :remit_request_result do
     association :user, factory: :user
     association :requested_user, factory: :user
-    amount 100
-    result 'accepted'
+    amount { 100 }
+    result { 'accepted' }
 
     trait :accepted do
     end
 
     trait :rejected do
-      result 'rejected'
+      result { 'rejected' }
     end
 
     trait :canceled do
-      result 'canceled'
+      result { 'canceled' }
     end
   end
 end
